88ace0f8fb7feef467d6c07d97cbe578ee24af4d
Sergey Lazarev <is.lazarev@icloud.com>
2024-03-10 23:09:38 +0300

    Source code changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,9 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
         },
+        mainnet-networks: {
+          sidebarPath: './sidebars/mainnetSidebar.js',
+        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
