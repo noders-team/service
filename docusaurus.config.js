b2b4bb7eb44c28f50d3e214c74e8939002d1eb97
Sergey Lazarev <is.lazarev@icloud.com>
2024-03-10 23:41:54 +0300

    Source code changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.solanamobile.com',
+  url: 'https://services.noders.team',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
