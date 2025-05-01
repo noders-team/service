c7fa09dc99637dcbc672468a4138e7f4e5d447ae
vbloher <vbloher7@gmail.com>
2025-05-01 14:17:45 +0200

    add hover effect to chain card

--- src/components/ChainCard/index.tsx
+++ src/components/ChainCard/index.tsx
@@ -9,7 +9,20 @@ interface ChainCardProps {
 
 function ChainCard({ chainName, chainId, iconUrl }: ChainCardProps) {
   return (
-    <Box display="flex" gap={2} sx={{ borderRadius: 2, p: 2 }} bgcolor={theme => theme.palette.background.paper}>
+    <Box
+      display="flex"
+      gap={2}
+      bgcolor={theme => theme.palette.background.paper}
+      borderRadius={2}
+      p={2}
+      sx={{
+        border: '1px solid transparent',
+        transition: theme => theme.transitions.create('border'),
+        '&:hover': {
+          border: theme => `1px solid ${theme.palette.primary.main}`
+        }
+      }}
+    >
       <Box
         component="img"
         src={iconUrl}
