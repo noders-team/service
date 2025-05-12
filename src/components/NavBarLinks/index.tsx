2821dea974f74171a1795eac1b28c29106f16b55
vbloher <vbloher7@gmail.com>
2025-05-12 23:22:01 +0200

    optimize theme provider

--- src/components/NavBarLinks/index.tsx
+++ src/components/NavBarLinks/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import MainnetIcon from "@site/static/img/mainnet-icon.svg"
-import TestnetIcon from "@site/static/img/testnet-icon.svg"
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import MainnetIcon from '@site/static/img/mainnet-icon.svg';
+import TestnetIcon from '@site/static/img/testnet-icon.svg';
 
 export default function NavBarLinks(props: any): React.JSX.Element {
   const isMobile = props?.mobile === true;
   const isDesktop = useMediaQuery('(min-width: 997px)');
-  const mainnetHref = useBaseUrl("mainnet-networks/overview");
-  const testnetHref = useBaseUrl("testnet-networks/overview");
+  const mainnetHref = useBaseUrl('mainnet-networks/overview');
+  const testnetHref = useBaseUrl('testnet-networks/overview');
 
   if (!isMobile && !isDesktop) {
     return null;
@@ -23,35 +23,51 @@ export default function NavBarLinks(props: any): React.JSX.Element {
       pl={isDesktop ? 6 : 1}
       pb={isDesktop ? 0 : 2}
       gap={isDesktop ? 6 : 1}
-      flexDirection={isDesktop ? "row" : "column"}
+      flexDirection={isDesktop ? 'row' : 'column'}
     >
-      <Box component={Link} href={mainnetHref} display="flex" alignItems="center" gap={1}
-           sx={{
-             color: "text.primary",
-             textDecoration: "none",
-             opacity: 0.6,
-             "&:hover": {
-               textDecoration: "none",
-               opacity: 1
-             }
-           }}
+      <Box
+        component={Link}
+        href={mainnetHref}
+        display="flex"
+        alignItems="center"
+        gap={1}
+        sx={{
+          color: 'inherit',
+          textDecoration: 'none',
+          opacity: 0.6,
+          transition: (theme) => theme.transitions.create('all'),
+          '&:hover': {
+            textDecoration: 'none',
+            opacity: 1,
+          },
+        }}
       >
         <MainnetIcon width={24} height={24} />
-        <Typography variant="subtitle1" sx={{fontWeight: 600}}>Mainnet</Typography>
+        <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+          Mainnet
+        </Typography>
       </Box>
-      <Box component={Link} href={testnetHref} display="flex" alignItems="center" gap={1}
-           sx={{
-             color: "text.primary",
-             textDecoration: "none",
-             opacity: 0.6,
-             "&:hover": {
-               textDecoration: "none",
-               opacity: 1
-             }
-           }}
+      <Box
+        component={Link}
+        href={testnetHref}
+        display="flex"
+        alignItems="center"
+        gap={1}
+        sx={{
+          color: 'inherit',
+          textDecoration: 'none',
+          opacity: 0.6,
+          transition: (theme) => theme.transitions.create('all'),
+          '&:hover': {
+            textDecoration: 'none',
+            opacity: 1,
+          },
+        }}
       >
         <TestnetIcon width={24} height={24} />
-        <Typography variant="subtitle1" sx={{fontWeight: 600}}>Testnet</Typography>
+        <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+          Testnet
+        </Typography>
       </Box>
     </Box>
   );
