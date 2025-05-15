28fc9b1acae1380d4c40875695c9f917f5b490f0
vbloher <vbloher7@gmail.com>
2025-05-15 20:48:39 +0200

    fix page title

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -19,17 +19,19 @@ function PageTitle({ iconUrl, title, chainId, version }: Props): React.JSX.Eleme
     <Box display="flex" flexDirection="column" gap={2}>
       <Box display="flex" alignItems="center" gap={2}>
         <Box component="img" src={baseIconUrl} alt={title} width={56} height={56} />
-        <Typography variant="h1">
-          {title}
-        </Typography>
+        <Typography variant="h1">{title}</Typography>
       </Box>
       {chainId && version && (
-        <Box display="flex" gap={2}>
-          <Typography variant="subtitle2">Chain ID:</Typography>
-          <CodeText text={chainId} />
+        <Box display="flex" flexWrap="wrap" gap={2}>
+          <Box display="flex" gap={2}>
+            <Typography variant="subtitle2">Chain ID:</Typography>
+            <CodeText text={chainId} />
+          </Box>
           <Divider orientation="vertical" flexItem />
-          <Typography variant="subtitle2">Current Node Version:</Typography>
-          <CodeText text={version} />
+          <Box display="flex" gap={2}>
+            <Typography variant="subtitle2">Current Node Version:</Typography>
+            <CodeText text={version} />
+          </Box>
         </Box>
       )}
     </Box>
