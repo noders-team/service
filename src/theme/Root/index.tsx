434c2f365d79be0779bdd6834c161e7260499427
vbloher <vbloher7@gmail.com>
2025-07-04 18:26:52 +0200

    wait for mui

--- src/theme/Root/index.tsx
+++ src/theme/Root/index.tsx
@@ -1,8 +1,18 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import MuiThemeProvider from '@/components/MuiThemeProvider';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
 
 export default function Root({ children }: { children: ReactNode }): React.JSX.Element {
+  const [isMuiReady, setIsMuiReady] = useState(false);
+
+  useEffect(() => {
+    setIsMuiReady(true);
+  }, []);
+
+  if (!isMuiReady) {
+    return <div>LOADING...</div>;
+  }
+
   return (
     <>
       <InitColorSchemeScript />
